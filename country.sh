807b2efc45c3a2ee88b662e96885e82ebe66a8ba
DustBunnykd <diamondayst300@gmail.com>
2025-12-06 12:33:09 -0500

    adds

---
+++ country.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mother="Mrs Bankole"
+bank="GTB"
+state="Lagos"
+
+echo ${mother} "worked at" ${bank} "in" ${state} "for over fifteen years"
+sleep 5
+echo "The most popular bank in" ${state} ${bank} "is currently recruting and" ${mother} "just got wind of it, and she has asked her last born to immediately apply to the bank."
+sleep 5
+echo "Among the states in Nigeria is" ${state} "they are lucky to have such a popular bank in Nigeria."
